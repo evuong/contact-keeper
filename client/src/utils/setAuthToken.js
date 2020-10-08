d98cbaa3fda576c6a7660f3b446803abcc81ad99
evuong <edwxrdv@gmail.com>
2020-10-08 11:38:28 -0400

    commit

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const setAuthToken = (token) => {
+  // if there is a token, set it
+  // else delete it
   if (token) {
     axios.defaults.headers.common['x-auth-token'] = token;
   } else {
