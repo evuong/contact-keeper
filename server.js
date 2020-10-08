91d5d318d30f29bb2ed09b2ed1655b0c62db9a78
evuong <edwxrdv@gmail.com>
2020-10-08 11:17:30 -0400

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// connect database
+connectDB();
+
+// initialize middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API' })
 );
 
-//define routes
+// define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
